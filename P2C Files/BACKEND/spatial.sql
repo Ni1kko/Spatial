b386ec9196b3887b3c6215141ca21ed9f10013ae
(Fallox) Nikko Renolds <ni1kko@outlook.com>
2021-12-24 11:42:14 +0000

    Created users table

--- P2C Files/BACKEND/spatial.sql
+++ P2C Files/BACKEND/spatial.sql
@@ -9,6 +9,24 @@ CREATE TABLE IF NOT EXISTS `guilds` (
   `updated_at` timestamp NULL DEFAULT NULL
 ) ENGINE=InnoDB AUTO_INCREMENT=0 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_bin;
 
+CREATE TABLE IF NOT EXISTS `users` (
+  `id` int(11) unsigned NOT NULL AUTO_INCREMENT,
+  `guild_id` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci NOT NULL,
+  `user_id` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci NOT NULL,
+  `role_id` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci NOT NULL,
+  `created_at` timestamp NULL DEFAULT NULL,
+  `updated_at` timestamp NULL DEFAULT NULL
+) ENGINE=InnoDB AUTO_INCREMENT=0 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_bin;
+
 ALTER TABLE `guilds`
   ADD PRIMARY KEY (`id`),
   ADD UNIQUE KEY `guild_id` (`guild_id`);
+
+ALTER TABLE `users`
+  ADD PRIMARY KEY (`id`),
+  ADD UNIQUE KEY `user_id` (`user_id`),
+  ADD KEY `users_guild_foriegnkey` (`guild_id`);
+
+ALTER TABLE `guilds`
+  ADD CONSTRAINT `users_guild_foriegnkey` FOREIGN KEY (`guild_id`) REFERENCES `guilds` (`guild_id`) ON DELETE NO ACTION ON UPDATE NO ACTION;
+COMMIT;
