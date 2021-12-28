93b958aac0564c9c24eb2dec85054bed7637a1b6
(Fallox) Nikko Renolds <ni1kko@outlook.com>
2021-12-28 23:18:42 +0000

    Update compress.bat

--- Release/tools/compress.bat
+++ Release/tools/compress.bat
@@ -2,5 +2,5 @@
 
 REM https://docs.microsoft.com/en-us/windows-server/administration/windows-commands/for
 FOR /F "delims=" %%i IN ("%~1") DO (
-	binarytocompressed.exe -base85 "%~1" %%~ni > %%~ni.cpp 
+	binarytocompressed.exe -base85 "%~1" %%~ni > %%~ni.h
 )
