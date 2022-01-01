9ab007eb88573755859a29a0cd9efff43013890a
(Fallox) Nikko Renolds <ni1kko@outlook.com>
2022-01-01 18:08:15 +0000

    Update Misc.cpp
    Remove whitespace

--- Source/Hacks/Misc.cpp
+++ Source/Hacks/Misc.cpp
@@ -1391,7 +1391,6 @@ static void from_json(const json& j, MiscConfig::SpectatorList& sl)
     read<value_t::object>(j, "Size", sl.size);
 }
  
-
 static void from_json(const json& j, PreserveKillfeed& o)
 {
     read(j, "Enabled", o.enabled);
