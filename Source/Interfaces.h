a019076e2cc5345f45ce8b2fca31f52640bb0828
(Fallox) Nikko Renolds <ni1kko@outlook.com>
2022-01-04 19:58:57 +0000

    Update Interfaces.h

--- Source/Interfaces.h
+++ Source/Interfaces.h
@@ -46,7 +46,7 @@ type* name = reinterpret_cast<type*>(find(moduleName, version));
     GAME_INTERFACE(EntityList, entityList, CLIENT_DLL, "VClientEntityList003")
     GAME_INTERFACE(GameEventManager, gameEventManager, ENGINE_DLL, "GAMEEVENTSMANAGER002")
     GAME_INTERFACE(GameMovement, gameMovement, CLIENT_DLL, "GameMovement001")
-    GAME_INTERFACE(GameUI, gameUI, CLIENT_DLL, "GameUI011") // you may enable it to use GameUI::createCommandMsgBox()
+    GAME_INTERFACE(GameUI, gameUI, CLIENT_DLL, "GameUI011")
     GAME_INTERFACE(InputSystem, inputSystem, INPUTSYSTEM_DLL, "InputSystemVersion001")
     GAME_INTERFACE(Localize, localize, LOCALIZE_DLL, "Localize_001")
     GAME_INTERFACE(MaterialSystem, materialSystem, MATERIALSYSTEM_DLL, "VMaterialSystem080")
@@ -64,14 +64,47 @@ type* name = reinterpret_cast<type*>(find(moduleName, version));
 
 #undef GAME_INTERFACE
 private:
+    static std::string getInterfaceDllName(const char* interfacename)  noexcept
+    {
+        if (interfacename == "VBaseFileSystem011")
+            return std::string{ FILESYSTEM_DLL }.append(".dll");
+        else if (interfacename == "VEngineCvar007")
+            return std::string{ VSTDLIB_DLL }.append(".dll");
+        else if (interfacename == "InputSystemVersion001")
+            return std::string{ INPUTSYSTEM_DLL }.append(".dll");
+        else if (interfacename == "Localize_001")
+            return std::string{ LOCALIZE_DLL }.append(".dll");
+        else if (interfacename == "VMaterialSystem080")
+            return std::string{ MATERIALSYSTEM_DLL }.append(".dll");
+        else if (interfacename == "PanoramaUIEngine001")
+            return std::string{ PANORAMA_DLL }.append(".dll");
+        else if (interfacename == "VPhysicsSurfaceProps001")
+            return std::string{ VPHYSICS_DLL }.append(".dll");
+        else if (interfacename == "VGUI_Surface031")
+            return std::string{ VGUIMATSURFACE_DLL }.append(".dll");
+        else if (interfacename == "VSoundEmitter003")
+            return std::string{ SOUNDEMITTERSYSTEM_DLL }.append(".dll");
+        else if (interfacename == "VStudioRender026")
+            return std::string{ STUDIORENDER_DLL }.append(".dll");
+        else if (interfacename == "VClient018" || interfacename == "VClientEntityList003" || interfacename == "GameMovement001" || interfacename == "GameUI011" || interfacename == "VClientPrediction001")
+            return std::string{ CLIENT_DLL }.append(".dll");
+        else if (interfacename == "VEngineClient014" || interfacename == "EngineTraceClient004" || interfacename == "GAMEEVENTSMANAGER002" || interfacename == "VModelInfoClient004" || interfacename == "VEngineModel016" || interfacename == "VEngineClientStringTable001" || interfacename == "VEngineRenderView014" || interfacename == "IEngineSoundClient003")
+            return std::string{ ENGINE_DLL }.append(".dll");
+        else
+            return std::string{ "Undefined DLL" };
+    }
+
     static void* find(const char* moduleName, const char* name) noexcept
     {
         if (const auto createInterface = reinterpret_cast<std::add_pointer_t<void* __CDECL(const char* name, int* returnCode)>>(GetProcAddress(GetModuleHandleA(moduleName), "CreateInterface"))) {
             if (void* foundInterface = createInterface(name, nullptr))
                 return foundInterface;
         }
+        
+        auto title = Helpers::getDllNameVersion();
+        auto message = std::string{ "Failed to find interface(" }.append(name).append(") [").append(getInterfaceDllName(name)).append("]");
 
-        Helpers::messageBox("Spatial", ("Failed to find " + std::string{ name } + " interface!").c_str());
+        Helpers::messageBox(title.c_str(), message.c_str());
         std::exit(EXIT_FAILURE);
     }
 };
